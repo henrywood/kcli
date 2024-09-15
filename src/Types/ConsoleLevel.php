f5a59867e25f51e416ab0123cb035ab0a7be1bf2
henrywood <henry.wood.dk@gmail.com>
2024-09-15 11:52:32 +0200

    Update ConsoleLevel.php

--- src/Types/ConsoleLevel.php
+++ src/Types/ConsoleLevel.php
@@ -5,12 +5,12 @@ final class ConsoleLevel
 {
     private string $value;
     
-    const NORMAL = "\e[0m";
-    const INFO = "\e[94m";
-    const WARNING = "\e[93m";
-    const ERROR = "\e[93m";
-    const SUCCESS = "\e[91m";
-    const HIGHLIGHT = "\e[1;95m";
+    private const NORMAL = "\e[0m";
+    private const INFO = "\e[94m";
+    private const WARNING = "\e[93m";
+    private const ERROR = "\e[93m";
+    private const SUCCESS = "\e[91m";
+    private const HIGHLIGHT = "\e[1;95m";
 
     private function __construct(string $val) {
         $this->value = $val;
